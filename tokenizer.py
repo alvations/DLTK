eb81a0512b7a9ed40c7e71cd9b0f31ae698e66b7
alvations <alvations@gmail.com>
2013-11-07 13:32:28 +0100

    Added abbreviation hack to train_punktsent

--- tokenizer.py
+++ tokenizer.py
@@ -3,7 +3,8 @@
 import sys; reload(sys); sys.setdefaultencoding("utf-8")
 import codecs, re, os, tempfile
 import cPickle as pickle
-from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
+from nltk.tokenize.punkt import PunktTrainer, \
+PunktSentenceTokenizer,PunktParameters
 
 DEUPUNCT = u""",–−—’‘‚”“‟„! £"%$'&)(+*-€/.±°´·¸;:=<?>@§#¡•[˚]»_^`≤…\«¿¨{}|"""
 
@@ -15,7 +16,7 @@ def txt2tmp(text):
 def punct_tokenize(text):
   """ Tokenize by simply adding spaces before and after punctuations. """
   rx = re.compile('[%s]' % re.escape(DEUPUNCT), re.UNICODE)
-  return rx.sub(ur" \g<0> ", text).split()
+  return [i.split() for i in rx.sub(ur" \g<0> ", text).split("\n")]
 
 def rb_tokenize(text): # Source: http://goo.gl/sF5WA5
   """ Tokenize using a rule-base perl script by Stefanie Dipper."""
@@ -39,8 +40,13 @@ def train_punktsent(trainfile, modelfile):
       punkt.train(fin.read(), finalize=False, verbose=False)
   except KeyboardInterrupt:
     print 'KeyboardInterrupt: Stopping the reading of the dump early!'
+  ##HACK: Adds abbreviations from rb_tokenizer.
+  abbrv_sent = " ".join([i.strip() for i in \
+                         codecs.open('abbrev.lex','r','utf8').readlines()])
+  abbrv_sent = "Start"+abbrv_sent+"End."
+  punkt.train(abbrv_sent,finalize=False, verbose=False)
+  # Finalize and outputs trained model.
   punkt.finalize_training(verbose=True)
-  # Outputs trained model.
   model = PunktSentenceTokenizer(punkt.get_params())
   with open(modelfile, mode='wb') as fout:
     pickle.dump(model, fout, protocol=pickle.HIGHEST_PROTOCOL)
@@ -50,14 +56,20 @@ def deupunkt_tokenize(text):
   """ Modifying the unsupervised punkt algorithm in NLTK for German. """
   try:
     with open('1000deu.pickle', mode='rb') as fin:
-      sent_tokenize = pickle.load(fin) 
+      sent_tokenizer = pickle.load(fin)
+    # Adds DEUPUNCT from global variable. 
+    sent_tokenizer.PUNCTUATION+=tuple(DEUPUNCT)
   except(IOError, pickle.UnpicklingError):
-    sent_tokenize = text.split("\n")
-  return sent_tokenize.tokenize(text)
+    sent_tokenizer = text.split("\n") # Backoff with "\n" as delimiter
+  return [i.split() for i in sent_tokenizer.tokenize(text)]
+  
+sent = u"""„Frau Präsidentin! Ist meine Stimme mitgezählt worden?""" 
+sent+=u"""Of course this a.M., it is Bros.“"""
 
-sent = """„Frau Präsidentin! Ist meine Stimme mitgezählt worden?“"""
+train_punktsent('1000.de','1000deu.pickle')
 
 print koehn_tokenize(sent)
-
-
-
+print rb_tokenize(sent)
+print punct_tokenize(sent)
+print deupunkt_tokenize(sent)
+print 
